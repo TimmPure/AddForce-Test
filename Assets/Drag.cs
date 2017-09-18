3d77893e07f97a9c3f35e853c7ce451b20a217c1
TimmPure <T.j.m.voskuil@gmail.com>
2017-09-18 13:45:02 +0200

    Initial commit

---
+++ Assets/Drag.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Drag : MonoBehaviour {
+
+    private Vector3 mousePos;
+    private bool isDragging = false;
+
+    void Update() {
+        mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+
+        if (Input.GetMouseButtonDown(0)) {
+            RaycastHit2D hit = Physics2D.Raycast(mousePos, Vector2.zero);
+            if (hit.collider != null && hit.transform.gameObject.name == "Sphere") {
+                isDragging = true;
+            }
+        } else if (Input.GetMouseButtonUp(0) && isDragging) {
+            isDragging = false;
+            Impulse.jumpVector = (transform.position - mousePos).normalized;
+            Impulse.triggerJump = true;
+        }
+    }
+}
---
+++ Assets/Impulse.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Impulse : MonoBehaviour {
+
+    private Rigidbody2D body;
+    private float power = 15f;
+
+    public static Vector2 jumpVector;
+    public static bool triggerJump;
+
+    private void Start() {
+        body = GetComponent<Rigidbody2D>();
+        triggerJump = false;
+    }
+
+    void FixedUpdate() {
+        if (triggerJump) {
+            body.AddForce(jumpVector * power, ForceMode2D.Impulse);
+            triggerJump = false;
+        }
+    }
+}
